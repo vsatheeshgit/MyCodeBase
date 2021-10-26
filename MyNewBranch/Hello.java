092f2855e49760b1aaf3c8961e94d13d8525d05f
Satheeshkumar Venkatachalam <satheeshkumar.venkatachalam@hp.com>
2021-10-26 14:23:12 +0530

    change

--- MyNewBranch/Hello.java
+++ MyNewBranch/Hello.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class Hello{
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
